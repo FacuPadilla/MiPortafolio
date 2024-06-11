6d568c89634b9204cec3ef0a54eae7d60780b728
Facundo <facupadilla19904@gmail.com>
2024-06-11 14:13:35 -0300

    actu 2

--- src/components/Projectos.jsx
+++ src/components/Projectos.jsx
@@ -97,19 +97,19 @@ The application allows tattoo artists to showcase their art, availability, and s
                  <div className="border-1 rounded-lg  *:dark:bg-slate-800
                  m-5 transform transition  hover:scale-105">
                     <div className="bg-white ">
-                        <img src="https://raw.githubusercontent.com/FacuPadilla/MiPortafolio/main/src/img/dashb.png" alt=""
+                        <img src="https://raw.githubusercontent.com/FacuPadilla/MiPortafolio/main/src/img/Screenshot%202024-06-11%20120304.png" alt=""
                         className="opacity-70 hover:opacity-100" />
-                        <h4 className="font-bold my-2 ml-2">FOOD DELIVERY - DASHBOARD</h4>
+                        <h4 className="font-bold my-2 ml-2">QUIZ APP - GAME </h4>
                         <article className="flex justify-around">
                             <div className="flex flex-nowrap items-center mb-2">
-                                <span className="bg-slate-200 rounded-lg px-2 mr-2 text-slate-700">HTML</span>
-                                <span className="bg-slate-200 rounded-lg px-2 mr-2 text-slate-700">TAILWINDS</span>
                                 <span className="bg-slate-200 rounded-lg px-2 mr-2 text-slate-700">REACT</span>
+                                <span className="bg-slate-200 rounded-lg px-2 mr-2 text-slate-700">TAILWINDS CSS</span>
+                                <span className="bg-slate-200 rounded-lg px-2 mr-2 text-slate-700">HTML</span>
                                 <span className="bg-slate-200 rounded-lg px-2 mr-2 text-slate-700">JS</span>
                             </div>
                             <div className="text-xl text-slate-600 dark:text-slate-200 pr-2 flex flex-nowrap p-2">
-                                <a href="https://github.com/FacuPadilla/dashboard-store-1"><i className="bi bi-github px-2"></i></a>
-                                <a href="https://dashboard-store-1-vert.vercel.app/"><i className="bi bi-eye px-2"></i></a>
+                                <a href="https://github.com/FacuPadilla/cuestionario"><i className="bi bi-github px-2"></i></a>
+                                <a href="https://cuestionario-lemon.vercel.app/"><i className="bi bi-eye px-2"></i></a>
                             </div>
                         </article>
                         
@@ -117,9 +117,9 @@ The application allows tattoo artists to showcase their art, availability, and s
                         
                     </div>
                     <p className="p-4 text-center mt-2 rounded-lg bg-white" >
-                        <span className="font-bold">Food delivery - Personal project. </span>
+                        <span className="font-bold">Quiz App - Personal project. </span>
                         
-100% responsive design of a possible app dedicated to food orders and deliveries. Ensuring an optimal user experience on a wide variety of devices and screens.
+                        This application is a quiz on multiple topics divided by categories with questions related to general culture.
                         </p>
                 </div>
 
